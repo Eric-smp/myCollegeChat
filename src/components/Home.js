3a09975734ab4cbaa5947de26823605bb91aed7e
Smp <ericsmp18@gmail.com>
2023-09-20 19:07:02 -0300

    firts commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+
+const Home = () => {
+  return (
+    <div className='contentHome'>
+        <h1>Olá mundo </h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur maxime assumenda beatae ratione debitis animi culpa, sint placeat voluptatum nam optio aspernatur minus natus dolorem numquam ut magnam obcaecati. Aperiam.</p>
+    </div>
+  )
+}
+
+export default Home
